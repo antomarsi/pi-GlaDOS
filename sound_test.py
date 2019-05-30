95b877c1ecc0994eec79f63a1610b27ff4cd5cca
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-05-30 11:47:34 -0300

    Atualizado projeto

--- sound_test.py
+++ sound_test.py
@@ -1,10 +1,19 @@
-import pyttsx3
+import speake3
+from random import randint
 
-engine = pyttsx3.init()
+engine = speake3.Speake()
+text = "Hello, and, again, welcome to the Aperture Science Computer-aided Enrichment Center"
+text = text.split()
+words = []
+for word in text:
+    words.append("<prosody pitch=\"%d\">%s</prosody>" % (randint(0, 100)-50, word))
+text = " ".join(words)
+print (text)
 
-voices = engine.getProperty('voices')
-
-print(voices)
-
-engine.say("I will speak this text")
-engine.runAndWait()
+engine = speake3.Speake()  # Initialize the speake engine
+engine.set('voice', 'en+f3')
+engine.set('speed', '180')
+engine.set('pitch', '60')
+engine.set('markup')
+engine.say(text)  # String to be spoken
+engine.talkback()
