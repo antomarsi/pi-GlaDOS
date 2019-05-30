b2031bb22c2d19d3df6d731e6aa5623813038808
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-05-30 14:52:52 -0300

    Testing more voices sutff

--- sound_test.py
+++ sound_test.py
@@ -1,19 +1,19 @@
 import speake3
 from random import randint
 
-engine = speake3.Speake()
-text = "Hello, and, again, welcome to the Aperture Science Computer-aided Enrichment Center"
-text = text.split()
-words = []
-for word in text:
-    words.append("<prosody pitch=\"%d\">%s</prosody>" % (randint(0, 100)-50, word))
-text = " ".join(words)
-print (text)
+text = '<prosody pitch=\"-2\">Hello,</prosody> <prosody pitch=\"+31%\">and,</prosody> <prosody pitch=\"-18\">again,</prosody> <prosody pitch=\"-44\">welcome</prosody> <prosody pitch=\"-15\">to</prosody> <prosody pitch=\"-43\">the</prosody> <emphasis level=\"strong\"><prosody pitch=\"-18\">Aperture</prosody> <prosody pitch=\"+23\">Science</prosody> <prosody pitch=\"+39\">Computer-aided</prosody> <prosody pitch=\"+41\">Enrichment</prosody> <prosody pitch=\"-10\">Center</prosody></emphasis>'
+
 
 engine = speake3.Speake()  # Initialize the speake engine
 engine.set('voice', 'en+f3')
 engine.set('speed', '180')
-engine.set('pitch', '60')
 engine.set('markup')
 engine.say(text)  # String to be spoken
 engine.talkback()
+# espeak -m -ven+f2 -s 180 -p 60 "<speak><s><prosody rate=\"x-slow\">Oh</prosody> <break time=\"1s\"/>, <prosody rate=\"slow\">i</prosody> <prosody rate=\"slow\">see</prosody> <prosody rate=\"slow\">you</prosody></s></speak>"
+#   for a in `cat`; do
+#     V=$(((($RANDOM) % 100) - 50))
+#     echo -n "<prosody pitch=\"+$V\">$a</prosody> " |
+#       sed 's/+-/-/' 
+#   done |
+#   espeak -ven+f3 -m -p 20 -s 180
