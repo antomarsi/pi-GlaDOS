f769dc930a7546c7bb7c281cd3ce9bfbe555a980
Antônio Marco da Silva <antomarsi@hotmail.com>
2021-12-03 00:00:33 -0300

    added spotify support

--- glados.py
+++ glados.py
@@ -1,3 +1,4 @@
+from random import random
 from gladosTTS import GladosTTS
 import speech_recognition as sr
 from pocketsphinx import LiveSpeech, get_model_path
@@ -6,12 +7,35 @@ import os
 import psutil
 import sys
 import datetime as dt
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import spotipy.util as util
+from flask import Flask
 
 model_path = get_model_path()
-
+CACHE = '.spotipyoauthcache'
 class Glados:
     def __init__(self) -> None:
+        self.app = Flask("Glados")
         self.tts = GladosTTS()
+        scope = [
+            "user-read-playback-state",
+            "user-modify-playback-state",
+            "streaming",
+            "app-remote-control",
+            "playlist-read-private",
+            "playlist-modify-public"
+        ]
+        token = spotipy.util.prompt_for_user_token(
+            os.getenv("SPOTIPY_USERNAME"),
+            client_id=os.getenv("SPOTIFY_ID"),
+            client_secret=os.getenv("SPOTIFY_SECRET"),
+            scope=" ".join(scope),
+            redirect_uri="http://localhost:8080/callback",
+            cache_path=CACHE
+        )
+        self.sp = spotipy.Spotify(auth=token)
+        self.selected_device = self.last_device_id()
 
     def restart_program():
         try:
@@ -38,7 +62,6 @@ class Glados:
                     lm=False
                 )
                 for phrase in speech:
-                    
                     try:
                         # Listen for command
                         command = self.take_command()
@@ -48,18 +71,32 @@ class Glados:
                         # Something failed
                         print(e)
             else:
-                try:
-                        # Listen for command
-                    command = self.take_command()
-                    # Execute command
-                    self.process_command(command)
-                except Exception as e:
-                    # Something failed
-                    print(e)
+                while(True):
+                    try:
+                            # Listen for command
+                        command = self.take_command()
+                        # Execute command
+                        self.process_command(command)
+                        time.sleep(2)
+                    except Exception as e:
+                        print(e)
             print("finished")
 
-    def take_command(self):
+    def last_device_id(self):
+        devices = self.sp.devices()
+        current_device = None
+        for device in devices["devices"]:
+            if device["is_active"] is True:
+                current_device = device["id"]
+                break
+        if current_device is None:
+            current_device = devices["devices"][0]["id"]
+        return current_device
 
+
+
+    def take_command(self):
+ 
         # Feedback to user that GLaDOS is listening
         print('listening...')
         time.sleep(0.3)
@@ -87,11 +124,6 @@ class Glados:
                 command = command.lower()
 
                 print("I heard: "+command)
-
-                # Save input as file as later training data
-                #timestamp = str(int(dt.datetime.now().timestamp()))
-                # with open("/home/nerdaxic/GLaDOS/collectedSpeech/" + timestamp+" "+command+".wav", "wb") as f:
-                #	f.write(voice.get_wav_data(convert_rate=16000))
 
                 # Remove possible trigger word from input
                 if os.getenv('TRIGGERWORD') in command:
@@ -178,7 +210,87 @@ class Glados:
         elif 'restart' in command or 'reload' in command:
             self.tts.speak("Cake and grief counseling will be available at the conclusion of the test.")
             self.restart_program()
+        
+        # Spotify
+        elif "next song" in command:
+            self.tts.speak("Playing next song")
+            self.sp.next_track()
 
+        elif "previous  song" in command:
+            self.tts.speak("Playing previous song")
+            self.sp.previous_track()
+
+        elif "pause" in command:
+            self.tts.speak("Pausing")
+            self.sp.pause_playback()
+            
+        # Play Track
+        elif "play" in command and "album" in command:
+            query = command.replace('play', '')
+            query = query.replace('please', '')
+            query = query.replace('album', '')
+            query = query.replace('  ', '')
+            try:
+                results = self.sp.search(query, type='album', limit=1)
+                for i, t in enumerate(results['albums']['items']):
+                    uri = t['uri']
+                self.tts.playDefault('clock_ding_on')
+                current = self.sp.start_playback(device_id=self.selected_device, context_uri=uri)
+                if len(uri) == 0:
+                    self.tts.speak("No album found, your memory is failing you")
+            except:
+                self.tts.speak("No album found, your memory is failing you")
+                print('No album found')
+        elif "play" in command and "playlist" in command:
+                query = command.replace('playlist', '')
+                query = query.replace('please', '')
+                query = query.replace('play', '')
+                query = query.replace('  ', '')
+                try:
+                    results = self.sp.search(query, type='playlist', limit=1)
+                    for i, t in enumerate(results['playlists']['items']):
+                        uri = t['uri']
+                    self.tts.playDefault('clock_ding_on')
+                    current = self.sp.start_playback(device_id=self.selected_device, context_uri=uri)
+                    if len(uri) == 0:
+                        self.tts.speak("No playlist found")
+                except:
+                    self.tts.speak("No playlist found")
+                    print('No playlist found')
+
+        elif "play" in command:
+            print("playing music")
+            query = command.replace('play', '')
+            query = query.replace('please', '')
+            query = query.replace('  ', '')
+
+            try:
+                uris = []
+                splited = query.split(" from ")
+                results = None
+                name = splited[0].strip()
+                artist = splited[1].strip()
+                if len(splited) > 1:
+                    new_query = "track:"+name+" artist:" +artist
+                    print("searching using artist and track")
+                    print(new_query)
+                    results = self.sp.search(new_query, type='track', limit=10)
+                    print(results)
+                if results is None:
+                    results = self.sp.search(query, type='track', limit=10)
+                for i, t in enumerate(results['tracks']['items']):
+                    uri = t['uri']
+                    uris.append(uri)
+                    break
+                self.tts.playDefault('clock_ding_on')
+                print(uris)
+                self.sp.start_playback(device_id=self.selected_device, uris=uris)
+                if len(uris) == 0:
+                    self.tts.speak('No track found, maybe you should study more about culture')
+            except Exception as e:
+                print(e)
+                self.tts.speak('No track found, maybe you should study more about culture')
+                print('No track found')
         else:
             print("Command not recognized")
             self.tts.playRandom("rec-fail")
@@ -186,5 +298,4 @@ class Glados:
             failList = open("failedCommands.log", "a")
             failList.write('\n'+command);
             failList.close()
-
         print("Waiting for trigger...")
